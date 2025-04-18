1e76cea6c58bcd3f4f4bd0c1794a632fc26d3b16
bandwagonhq <hq@bandwagonhq.app>
2025-04-17 22:36:20 -0500

    Log environment variables for debugging
    Log environment variables for debugging

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,11 @@
 export { GET, POST } from 'app/auth';
+console.log("==== ENV VARS BEGIN ====");
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("SUPABASE_ANON_KEY:", process.env.SUPABASE_ANON_KEY);
+console.log("AUTH_SECRET:", process.env.AUTH_SECRET);
+console.log("POSTGRES_URL:", process.env.POSTGRES_URL);
+console.log("POSTGRES_PRISMA_URL:", process.env.POSTGRES_PRISMA_URL);
+console.log("POSTGRES_URL_NON_POOLING:", process.env.POSTGRES_URL_NON_POOLING);
+console.log("==== ENV VARS END ====");
